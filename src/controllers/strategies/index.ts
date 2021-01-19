339bc00e6954a97f6fefce84a473efec4884be65
Etienne Schmitz <hello@etienne-schmitz.com>
2021-01-19 08:53:29 +0100

    add zigzag files

--- src/controllers/strategies/index.ts
+++ src/controllers/strategies/index.ts
@@ -1,5 +1,9 @@
 import Triangle from './triangle'
+import UseZigZag from './useZigZag'
+import ZigZag from './zigZag'
 
 export default {
   triangle: Triangle.declaration,
+  useZigZag: UseZigZag.declaration,
+  zigZag: ZigZag.declaration,
 }
